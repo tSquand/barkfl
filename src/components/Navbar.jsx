af2512a4dd7fc3f0c51a7393282f517fb9649a38
Tyler Sanders <tsanders5902@gmail.com>
2023-10-10 13:23:50 -0400

    navbar collapse on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,77 +1,101 @@
 import "./styles.css";
+import React, { useState } from "react";
 
 function Navbar({ handleNavLinkClick }) {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  const closeNav = () => {
+    setIsNavOpen(false);
+  };
+
   return (
-    <>
-      <nav className='navbar navbar-expand-lg bg-body-tertiary'>
-        <div className='container-fluid'>
-          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-            <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-              <li className='nav-item'>
-                <a
-                  className='nav-link'
-                  href='#'
-                  data-target='hours'
-                  onClick={handleNavLinkClick}
-                >
-                  Hours & Location
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a
-                  className='nav-link'
-                  href='#'
-                  data-target='about'
-                  onClick={handleNavLinkClick}
-                >
-                  About Us
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a
-                  className='nav-link'
-                  href='#'
-                  data-target='menu'
-                  onClick={handleNavLinkClick}
-                >
-                  Menu
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a
-                  className='nav-link'
-                  href='#'
-                  data-target='catering'
-                  onClick={handleNavLinkClick}
-                >
-                  Catering
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a
-                  className='nav-link'
-                  href='#'
-                  data-target='events'
-                  onClick={handleNavLinkClick}
-                >
-                  Upcoming Events
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a
-                  className='nav-link'
-                  href='#'
-                  data-target='contact'
-                  onClick={handleNavLinkClick}
-                >
-                  Contact Us
-                </a>
-              </li>
-            </ul>
-          </div>
+    <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+      <div className='container-fluid'>
+        {/* Brand/logo */}
+        <a className='navbar-brand' href='#'>
+          The Bark
+        </a>
+
+        {/* Toggle button for mobile */}
+        <button className='navbar-toggler' type='button' onClick={toggleNav}>
+          <span className='navbar-toggler-icon'></span>
+        </button>
+
+        {/* Collapsible content */}
+        <div className={`collapse navbar-collapse ${isNavOpen ? "show" : ""}`}>
+          <ul
+            className={`navbar-nav me-auto mb-2 mb-lg-0 ${
+              isNavOpen ? "centered-nav-items" : ""
+            }`}
+          >
+            <li className='nav-item'>
+              <a
+                className='nav-link'
+                href='#'
+                data-target='hours'
+                onClick={handleNavLinkClick}
+              >
+                Hours & Location
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a
+                className='nav-link'
+                href='#'
+                data-target='about'
+                onClick={handleNavLinkClick}
+              >
+                About Us
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a
+                className='nav-link'
+                href='#'
+                data-target='menu'
+                onClick={handleNavLinkClick}
+              >
+                Menu
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a
+                className='nav-link'
+                href='#'
+                data-target='catering'
+                onClick={handleNavLinkClick}
+              >
+                Catering
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a
+                className='nav-link'
+                href='#'
+                data-target='events'
+                onClick={handleNavLinkClick}
+              >
+                Upcoming Events
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a
+                className='nav-link'
+                href='#'
+                data-target='contact'
+                onClick={handleNavLinkClick}
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
 
