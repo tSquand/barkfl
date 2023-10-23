567fa84bf098dd153d49d93f5ba5a28a6f506ddf
Tyler Sanders <tsanders5902@gmail.com>
2023-10-23 11:02:17 -0400

    added facebook and instagram icons/links in contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,30 @@ function Contact({ address }) {
       <div className='row align-items-start'>
         <div>
           <h3>FOLLOW US</h3>
-          <div>ig symbol + fb symbol and links</div>
+          <div>
+            <a
+              href='https://www.instagram.com/thebarkfl/?hl=en'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img
+                src='logo-instagram.svg'
+                className='icon'
+                alt='Instagram Logo'
+              ></img>
+            </a>
+            <a
+              href='https://www.facebook.com/TheBarkFL'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img
+                src='logo-facebook.svg'
+                className='icon'
+                alt='Facebook Logo'
+              ></img>
+            </a>
+          </div>
           <h3>PHONE</h3>
           <p>
             <a className='phone-link' href={`tel:${phoneNumber}`}>
