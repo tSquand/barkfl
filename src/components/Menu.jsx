2171e32996d954cfbef05ec7af7b4faa972962c2
Tyler Sanders <tsanders5902@gmail.com>
2024-01-17 16:44:05 -0500

    added meta description, menu header with icons

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,14 +7,34 @@ function Menu() {
   return (
     <div>
       <div className='container text-center'>
+        <h1 className='menu-heading'>
+          Specials posted daily on{" "}
+          <a
+            href='https://www.instagram.com/thebarkfl/?hl=en'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              src='logo-instagram.svg'
+              className='icon'
+              alt='Instagram'
+            ></img>
+          </a>{" "}
+          and{" "}
+          <a
+            href='https://www.facebook.com/TheBarkFL'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img src='logo-facebook.svg' className='icon' alt='Facebook'></img>
+          </a>
+        </h1>
         <iframe
           title='PDF Viewer'
           src={pdfUrl}
           width='80%'
           height='900px'
         ></iframe>
-      </div>
-      <div className='container text-center'>
         <iframe
           title='PDF Viewer'
           src={brunchUrl}
