e949a1f25dd4da158121b7532848178febefd517
Tyler Sanders <tsanders5902@gmail.com>
2024-03-30 10:50:21 -0400

    pdf in new tab test

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,19 +3,26 @@ import React from "react";
 function Menu() {
   const pdfUrl = import.meta.env.BASE_URL + "Bark-Menu.pdf#view=fitH";
 
-  const isMobile = window.innerWidth <= 768;
+  const openPdfInNewTab = (url) => {
+    window.open(url, "_blank");
+  };
 
+  const isMobile = window.innerWidth <= 768;
   return (
     <div>
-      <div>
-        <div className='container text-center'>
+      <div className='container text-center'>
+        {isMobile ? (
+          <button onClick={() => openPdfInNewTab(pdfUrl)}>
+            Open PDF in New Tab
+          </button>
+        ) : (
           <iframe
             title='PDF Viewer'
             src={pdfUrl}
-            width={isMobile ? "100%" : "80%"}
+            width='80%'
             height='900px'
           ></iframe>
-        </div>
+        )}
       </div>
     </div>
   );
