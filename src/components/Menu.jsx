4e2ec7693d780e60f8a133c4b46d31ace1bcb6d4
Tyler Sanders <tsanders5902@gmail.com>
2024-03-30 10:31:06 -0400

    removed view menu buttons from mobile mode, checking to see if appears properly on phones

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -16,35 +16,12 @@ function Menu() {
     <div>
       <div>
         <div className='container text-center'>
-          {isMobile ? (
-            <div>
-              <button
-                className='btn btn-primary'
-                onClick={() => openPdfInNewTab(pdfUrl)}
-              >
-                View Menu
-              </button>
-              <button
-                className='btn btn-primary'
-                onClick={() => openPdfInNewTab(brunchUrl)}
-              >
-                View Brunch Menu
-              </button>
-              <button
-                className='btn btn-primary'
-                onClick={() => openPdfInNewTab(lateNightUrl)}
-              >
-                View Late Night Menu
-              </button>
-            </div>
-          ) : (
-            <iframe
-              title='PDF Viewer'
-              src={pdfUrl}
-              width='80%'
-              height='900px'
-            ></iframe>
-          )}
+          <iframe
+            title='PDF Viewer'
+            src={pdfUrl}
+            width='80%'
+            height='900px'
+          ></iframe>
         </div>
       </div>
     </div>
